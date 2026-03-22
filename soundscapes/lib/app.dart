22b6765dbb8e42dbff19198c8df83f5ef4f8123d
zmarcher9 <zmarcher9@gmail.com>
2026-03-22 13:17:10 -0400

    Updated app.dart and main.dart code

---
+++ soundscapes/lib/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'core/constants/app_theme.dart';
+import 'providers/theme_provider.dart';
+import 'screens/home/home_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return MaterialApp(
+      title: 'Adaptive Focus Studio',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: themeProvider.themeMode,
+      home: const HomeScreen(),
+    );
+  }
+}
